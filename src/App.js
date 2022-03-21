9b6f6a55c8c9fc0d29b3d4f76fba28664919316f
Michael C <mikkdc@yahoo.co.uk>
2022-03-21 19:45:10 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import logo from "./brbdlogo.png"
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+
+  const [quote, setQuote] = useState("");
+
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const fetchQuote = async () => {
+    try {
+      setError(false);
+      const response = await fetch("https://breaking-bad-quotes.herokuapp.com/v1/quotes");
+      if (response.status !== 200) {
+        throw new Error("Sorry, something went wrong");
+      }
+      const data = await response.json();
+
+      setQuote(data[0]);
+
+    } catch (error) {
+      console.log("error: ", error);
+      setError({ error: true, message: error.message });
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+    console.log("useEffect worked");
+  }, []);
+  if(!quote ) {
+    return <h1>Please wait. Getting Quote....</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      
+
+      <div className="title">
+        <img src={logo}></img>        
+        <h1>Quote Generator</h1>
+        <button onClick={fetchQuote}>Get Quote</button>
+      </div>
+
+      
+
+      <div className="quote">
+        <h2>"{quote.quote}"</h2>
+      </div>
+
+      <div className="author">
+        <h3>- {quote.author} -</h3>
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
